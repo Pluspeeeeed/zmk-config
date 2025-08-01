e52b8bcc4dfc82da360a7ff420ff61e06f948fa2
James Tan <txrtanzi@gmail.com>
2025-08-01 11:09:20 +0800

    Add other layers of vf-config

--- config/handsdown/vf-config.h
+++ config/handsdown/vf-config.h
@@ -23,3 +23,27 @@
 #define HD_RM &kp COMMA      &mt LSHFT A     &mt LGUI E      &mt LALT I      &mt LCTRL H
 #define HD_RB &kp MINUS      &kp U           &kp O           &kp Y           &kp B
 #define HD_RH &lt l_sym SPACE &lt l_nav RETURN
+
+// Japanese "mode" (no adaptives, & L, C, X not used so repurpose to ん, Z, -)
+#define JP_Layer_label "HD-JP"
+#define JP_LT &kp MINUS       &kp W           &kp M           &kp G           &kp J
+#define JP_LM &mt RCTRL S    &mt RALT Z     &mt RGUI N     &mt RSHFT T    &kp K
+#define JP_LB &kp F           &kp P           &t_nn           &kp D           &kp V
+#define JP_LH                                                 _______         &lt l_nav R
+
+#define JP_RT _______        _______      _______      &kp LBKT        &kp RBKT
+#define JP_RM _______         &mt LSHFT A    &mt LGUI E     &mt LALT I     &mt LCTRL H
+#define JP_RB _______         &kp U           &kp O           &kp Y           &kp B
+#define JP_RH _______         _______
+
+
+#define l_akDOT_label "l_akDOT"
+#define DOT_LT _______        _______         _______         _______         _______
+#define DOT_LM _______        _______         _______         _______         _______
+#define DOT_LB _______        _______         _______         _______         _______
+#define DOT_LH                                                _______         _______
+
+#define DOT_RT &kp DLLR       &kp DOT         _______       _______       _______
+#define DOT_RM &caps_word     _______         _______         _______         _______
+#define DOT_RB _______        _______         _______         _______         _______
+#define DOT_RH _______        _______
